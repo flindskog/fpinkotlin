154ac4ee500edba7ed6c5ed6b6dadbd2b3a4d086
Fredrik Lindskog <fredrik.lindskog@tundra.com>
2023-01-06 09:12:13 +0100

    more

--- src/test/kotlin/chapter3/exercises/ex13/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex13/listing.kt
@@ -1,17 +1,27 @@
 package chapter3.exercises.ex13
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.foldLeft
+import chapter3.foldRight
+import chapter3.reverse
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import utils.SOLUTION_HERE
 
 // tag::init[]
 fun <A> append(a1: List<A>, a2: List<A>): List<A> =
-
-    SOLUTION_HERE()
+    foldRight(
+        a1,
+        a2
+    ) { a, acc -> Cons(a, acc) }
 // end::init[]
 
-fun <A> appendL(a1: List<A>, a2: List<A>): List<A> = SOLUTION_HERE()
+fun <A> appendL(a1: List<A>, a2: List<A>): List<A> =
+    foldLeft(
+        reverse(a1),
+        a2
+    ) { acc, a -> Cons(a, acc) }
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise13 : WordSpec({
